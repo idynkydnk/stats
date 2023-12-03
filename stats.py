5c5f6a6e75227ddbd9637e40790d46da353a49bd
Mark <marklyu@gmail.com>
2023-12-02 21:41:19 -0800

    main.css - added CSS style definition for a button delete_game.html - updated  to use buttons for Yes / No responses stats.py * added the same error checking to the update game function as the add game function ** winner must have higher or equal score to looser ** same name can't be used for multiple players

--- stats.py
+++ stats.py
@@ -145,8 +145,13 @@ def update(game_id):
         winner_score = request.form['winner_score']
         loser_score = request.form['loser_score']
 
+
         if not winner1 or not winner2 or not loser1 or not loser2 or not winner_score or not loser_score:
             flash('All fields required!')
+        elif int(winner_score) < int(loser_score):
+            flash('Winner score is less than loser score!')
+        elif winner1 == winner2 or winner1 == loser1 or winner1 == loser2 or winner2 == loser1 or winner2 == loser2 or loser1 == loser2:
+            flash('Two names are the same!')
         else:
             update_game(game_id, game[1], winner1, winner2, winner_score, loser1, loser2, loser_score, datetime.now(), game_id)
             return redirect(url_for('edit_games'))
