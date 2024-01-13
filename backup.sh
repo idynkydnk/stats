c45b38ea361bdc3c32b71df813524904f8c4dc68
Mark <marklyu@gmail.com>
2024-01-12 23:22:05 -0800

    Got the backup.sh script working

--- backup.sh
+++ backup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# Path to file to backup
+# Path to database file to backup
 FILE="/home/MarkL/stats/stats.db"
 
 # Backup directory
 BACKUPDIR="/home/MarkL/stats/backup"
 
-# Metadata file to store last modified date  
+# Metadata file to store last modified date
 METAFILE="$BACKUPDIR/metadata.txt"
 
 # If metadata file doesn't exist, create it
 if [ ! -f "$METAFILE" ]; then
-  touch "$METAFILE"
+  echo "No last modified date file found, creating $METAFILE"
+  echo "0" >> "$METAFILE"
 fi
 
 # Get last modified date of file
@@ -23,13 +24,16 @@ LAST_BACKUP=$(cat "$METAFILE")
 # Compare last modified and last backup dates
 if [ "$FILE_MODTIME" -gt "$LAST_BACKUP" ]; then
 
-  # File modified since last backup, so make new backup
-  BACKUPFILE="$BACKUPDIR/$(date +%Y%m%d%H%M%S)-$(basename $FILE)"
+  # Vacuum the database to reduce size
+  sqlite3 $FILE 'VACUUM;'
+  echo "Vacuumed the database $FILE"
+  # Create a backup
+  BACKUPFILE="$BACKUPDIR/$(date +%Y_%m_%d_%H%M)-$(basename $FILE)"
   cp "$FILE" "$BACKUPFILE"
- 
+
   # Update metadata file timestamp
   echo "$FILE_MODTIME" > "$METAFILE"
- 
+
   echo "Created backup of $FILE to $BACKUPFILE"
 
 else
